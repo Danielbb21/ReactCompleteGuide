ab9feb876a5cea1a7c641cf736b46836efdf1c0e
Daniel <danielbb2000@gmail.com>
2021-07-28 15:15:46 -0300

    finished module 9

--- Try/my-try/src/Components/UI/ErrorModal.js
+++ Try/my-try/src/Components/UI/ErrorModal.js
@@ -1,30 +1,38 @@
 import React from 'react';
+import ReactDOM from 'react-dom';
 import Card from './Card';
 import Button from './Button';
 import style from './ErrorModal.module.css';
 
 const ErrorModal = (props) => {
 
-    const handleClickError = ()=>{
+    const handleClickError = () => {
         props.onError(false)
     }
-    return (
-        <div>
-            <div className={style.backdrop} onClick = {handleClickError}/>
-        <Card>
+
+    const Backdrop = props => {
+        return <div className={style.backdrop} onClick={handleClickError} />
+    }
+    const ModalOverlay = props => {
+        return <Card>
             <div className={style.modal}>
-            <header className={style.header}>
-                <h2>{props.title}</h2>
-            </header>
-            <div className={style.content}>
-                <p>{props.message}</p>
-            </div>
-            <footer className={style.actions}>
-                <Button onClick = {handleClickError}>Close</Button>
-            </footer>
+                <header className={style.header}>
+                    <h2>{props.title}</h2>
+                </header>
+                <div className={style.content}>
+                    <p>{props.message}</p>
+                </div>
+                <footer className={style.actions}>
+                    <Button onClick={handleClickError}>Close</Button>
+                </footer>
             </div>
         </Card>
-        </div>
+    }
+    return (
+        <>
+            { ReactDOM.createPortal(<Backdrop onClick={handleClickError}/>, document.getElementById('backdrop-root'))}
+            { ReactDOM.createPortal(<ModalOverlay title = {props.title} message = {props.message} onClick={handleClickError}/>, document.getElementById('overlay-root'))}
+        </>
     )
 };
 
--- Try/my-try/src/Components/Users/AddUser.js
+++ Try/my-try/src/Components/Users/AddUser.js
@@ -1,44 +1,43 @@
-import React, { useState } from 'react';
+import React, { useState, useRef } from 'react';
 import Button from '../UI/Button';
 import Card from '../UI/Card';
 import ErrorModal from '../UI/ErrorModal';
 import style from './AddUSer.module.css';
 
 const AddUser = (props) => {
+    const nameInputRef = useRef();
+    const ageInputRef = useRef();
 
     const [hasError, setHasError] = useState(false);
     const [errorMessage, setErrorMessage] = useState('');
-    const [username, setUsername] = useState('');
-    const [age, setAge] = useState('');
+
     const addUserHandler = (event) => {
         event.preventDefault();
-        let emptyField = verifyFields(username, age);
+
+        const enteredName = nameInputRef.current.value;
+        const enteredAge = ageInputRef.current.value;
+
+        let emptyField = verifyFields(enteredName, enteredAge);
         if (emptyField) {
             setHasError(true);
             setErrorMessage('you not insert  all the filds');
-            
+
             return;
         }
-        if (+age < 1) {
+        if (+enteredAge < 1) {
             setHasError(true);
             setErrorMessage('Invalid age format');
             return;
         }
         const user = {
-            name: username,
-            age: age
+            name: enteredName,
+            age: enteredAge
         }
         props.onSaveUser(user)
-        setUsername('');
-        setAge('');
+        nameInputRef.current.value = '';
+        ageInputRef.current.value = '';
     }
-    const changeUsername = (event) => {
-        setUsername(event.target.value);
 
-    }
-    const changeAge = (event) => {
-        setAge(event.target.value);
-    }
     const verifyFields = (...args) => {
 
         let result = args.some((element) => {
@@ -46,20 +45,26 @@ const AddUser = (props) => {
         })
         return result;
     }
-    const handleError =(error) =>{
+    const handleError = (error) => {
         setHasError(error)
     }
     return (
         <div>
-            {hasError ?  <ErrorModal title="sommeting went worng" message= {errorMessage} onError = {handleError}/> : ''}
-           
+            {hasError ? <ErrorModal title="sommeting went worng" message={errorMessage} onError={handleError} /> : ''}
+
             <Card>
                 <form className={style.teste} onSubmit={addUserHandler}>
 
                     <label htmlFor="username">Username</label>
-                    <input value={username} id="username" type="text" onChange={changeUsername} />
+                    <input
+
+                        id="username"
+                        type="text"
+
+                        ref={nameInputRef}
+                    />
                     <label htmlFor="age">Age(Years)</label>
-                    <input value={age} id="age" type="number" onChange={changeAge} />
+                    <input id="age" type="number" ref={ageInputRef} />
                     <Button type="submit">Add User</Button>
 
                 </form>
