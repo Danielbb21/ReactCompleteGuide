81de820bec10ecc75aa5fc0acec27a6b30f6dfa4
Daniel <danielbb2000@gmail.com>
2021-08-02 18:56:41 -0300

    Module 17[uncompleted]

--- InitialProject/module-17--starting-project/src/components/Meals/AvailableMeals.js
+++ InitialProject/module-17--starting-project/src/components/Meals/AvailableMeals.js
@@ -1,36 +1,51 @@
 import Card from '../UI/Card';
 import MealItem from './MealItem/MealItem';
 import classes from './AvailableMeals.module.css';
+import { useCallback, useEffect, useState } from 'react';
 
-const DUMMY_MEALS = [
-  {
-    id: 'm1',
-    name: 'Sushi',
-    description: 'Finest fish and veggies',
-    price: 22.99,
-  },
-  {
-    id: 'm2',
-    name: 'Schnitzel',
-    description: 'A german specialty!',
-    price: 16.5,
-  },
-  {
-    id: 'm3',
-    name: 'Barbecue Burger',
-    description: 'American, raw, meaty',
-    price: 12.99,
-  },
-  {
-    id: 'm4',
-    name: 'Green Bowl',
-    description: 'Healthy...and green...',
-    price: 18.99,
-  },
-];
+
+
 
 const AvailableMeals = () => {
-  const mealsList = DUMMY_MEALS.map((meal) => (
+  const [isLoading, setIsLoadding] = useState(true);
+  const [food, setFood] = useState([]);
+  const [hasError, setHasError] = useState(false);
+
+  const fetchMeals = useCallback(async () => {
+    try {
+      setHasError(false);
+      const response = await fetch('https://tasks-6afd2-default-rtdb.firebaseio.com/meals.json');
+      if (!response.ok) {
+        throw new Error('Sommeting went Wrong');
+      }
+      const data = await response.json();
+      setIsLoadding(false);
+      
+      const meals = [];
+      for (var meal in data) {
+        meals.push({
+          id: Math.random().toString(),
+          name: data[meal].name,
+          description: data[meal].description,
+          price: data[meal].price
+        })
+
+      }
+
+      setFood(meals);
+    }
+    catch (err) {
+      setIsLoadding(false);
+      console.log(err.message);
+      setHasError(true);
+    }
+  }, []);
+  useEffect(() => {
+    fetchMeals();
+  }, [fetchMeals]);
+  
+
+  const mealsList = food.map((meal) => (
     <MealItem
       key={meal.id}
       id={meal.id}
@@ -42,6 +57,8 @@ const AvailableMeals = () => {
 
   return (
     <section className={classes.meals}>
+      {isLoading && !hasError && <p>Loadding meals...</p>}
+      {hasError && <p>Somment went wrong</p>}
       <Card>
         <ul>{mealsList}</ul>
       </Card>
